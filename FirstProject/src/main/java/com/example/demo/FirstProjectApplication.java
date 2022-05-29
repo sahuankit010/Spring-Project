6e1553a4132b720592f01ff3eaea70b6bafae9b1
sahuankit010 <32036503+sahuankit010@users.noreply.github.com>
2022-05-28 20:19:30 -0500

    initialization of project

---
+++ FirstProject/src/main/java/com/example/demo/FirstProjectApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectApplication.class, args);
+		
+		System.out.println("Hello Ankit");
+	}
+
+}
