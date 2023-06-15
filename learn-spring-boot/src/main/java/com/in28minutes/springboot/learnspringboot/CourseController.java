4ec0e41d42035600cce12d60c8c710b5acfac8bc
sahuankit010 <sahu.ankit010@gmail.com>
2023-06-15 12:31:39 -0500

    spring boot in 12 steps done

--- learn-spring-boot/src/main/java/com/in28minutes/springboot/learnspringboot/CourseController.java
+++ learn-spring-boot/src/main/java/com/in28minutes/springboot/learnspringboot/CourseController.java
@@ -7,14 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
 public class CourseController {
 	
 	@RequestMapping("/courses")
 	public List<Course> retrieveAllCourses(){
 		return Arrays.asList(
 				new Course(1, "Learn AWS", "in28minutes"),
-				new Course(2, "Learn Devops", "in28minutes")
+				new Course(2, "Learn DevOps", "in28minutes"),
+				new Course(3, "Learn Java", "in28minutes")
 				);
 	}
 
