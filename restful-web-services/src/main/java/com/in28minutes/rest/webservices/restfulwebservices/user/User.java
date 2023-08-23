732bd0671e66b5c44568ed282f92465e9c6f54b2
sahuankit010 <sahu.ankit010@gmail.com>
2023-08-23 02:48:10 -0500

    adding h2 db, inserting data

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -2,21 +2,25 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import java.time.LocalDate;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
+@Entity(name = "user_details")
 public class User {
 
+	@Id
+	@GeneratedValue
 	private Integer id;
 	
 	@Size(min = 2, message = "Name should have atleast 2 character")
-	@JsonProperty("user_name")
+//	@JsonProperty("user_name")
 	private String name;
 	
 	@Past(message = "Birth date should be in past")
-	@JsonProperty("birth")
+//	@JsonProperty("birth")
 	private LocalDate birthDate;
 	
 	public User(Integer id, String name, LocalDate birthDate) {
