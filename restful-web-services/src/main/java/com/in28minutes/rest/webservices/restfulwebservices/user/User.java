80c4d0639f14e0d105f426ff4f82a82df5e349a6
sahuankit010 <sahu.ankit010@gmail.com>
2023-08-22 20:00:32 -0500

    adding actuator, static and dynamic filtering using Json, HAL

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -2,6 +2,8 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -10,9 +12,11 @@ public class User {
 	private Integer id;
 	
 	@Size(min = 2, message = "Name should have atleast 2 character")
+	@JsonProperty("user_name")
 	private String name;
 	
 	@Past(message = "Birth date should be in past")
+	@JsonProperty("birth")
 	private LocalDate birthDate;
 	
 	public User(Integer id, String name, LocalDate birthDate) {
