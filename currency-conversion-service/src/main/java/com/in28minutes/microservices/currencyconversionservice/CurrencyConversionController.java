bd76a17c338b7a68996f30f7fc212a7e092e0ef8
sahuankit010 <sahu.ankit010@gmail.com>
2023-09-08 16:49:12 -0500

    adding currency-exchange-service

---
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,37 @@
+package com.in28minutes.microservices.currencyconversionservice;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CurrencyConversionController {
+
+	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateCurrencyConversion(
+			@PathVariable String from,
+			@PathVariable String to,
+			@PathVariable BigDecimal quantity
+			) {
+		
+				HashMap<String, String> uriVariables = new HashMap<>();
+				uriVariables.put("from", from);
+				uriVariables.put("to", to);
+				ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(
+						"http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+						CurrencyConversion.class, uriVariables);
+
+				CurrencyConversion currrencyConversion = responseEntity.getBody();
+
+				return new CurrencyConversion(currrencyConversion.getId(), from, to, quantity,
+						currrencyConversion.getConversionMultiple(),
+						quantity.multiply(currrencyConversion.getConversionMultiple()),
+						currrencyConversion.getEnvironment());
+	}
+
+}
---
+++ currency-conversion-service/src/main/java/com/in28minutes/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+}
