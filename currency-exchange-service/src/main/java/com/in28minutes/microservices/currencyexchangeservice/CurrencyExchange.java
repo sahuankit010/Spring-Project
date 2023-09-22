91e270785d764b95687f0c4b4dd39ad9cf47110f
sahuankit010 <sahu.ankit010@gmail.com>
2023-09-22 02:23:38 -0500

    starting with api-gateway

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -9,12 +9,15 @@ import jakarta.persistence.Id;
 @Entity
 public class CurrencyExchange {
 
-	@Column(name = "currency_from")
-	private String from;
-	@Column(name = "currency_to")
-	private String to;
 	@Id
 	private Long id;
+
+	@Column(name = "currency_from")
+	private String from;
+
+	@Column(name = "currency_to")
+	private String to;
+
 	private BigDecimal conversionMultiple;
 	private String environment;
 
@@ -24,11 +27,18 @@ public class CurrencyExchange {
 
 	public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
 		super();
+		this.id = id;
 		this.from = from;
 		this.to = to;
-		this.id = id;
 		this.conversionMultiple = conversionMultiple;
+	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public String getFrom() {
@@ -47,14 +57,6 @@ public class CurrencyExchange {
 		this.to = to;
 	}
 
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
 	public BigDecimal getConversionMultiple() {
 		return conversionMultiple;
 	}
@@ -62,6 +64,7 @@ public class CurrencyExchange {
 	public void setConversionMultiple(BigDecimal conversionMultiple) {
 		this.conversionMultiple = conversionMultiple;
 	}
+
 
 	public String getEnvironment() {
 		return environment;
--- currency-exchange-service/src/main/resources/data.sql
+++ currency-exchange-service/src/main/resources/data.sql
@@ -1,4 +1,3 @@
-
 insert into currency_exchange
 (id,currency_from,currency_to,conversion_multiple,environment)
 values(10001,'USD','INR',65,'');
